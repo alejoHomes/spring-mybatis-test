29145a84ac2fcf122ad91aae0b80906dfc999b3f
Alejandro Homes <ahomes@nisum.com>
2015-08-12 17:36:47 -0300

    add the sql scrip file to create the tables in postgresql

---
+++ create user_detail and department.sql
@@ -0,0 +1,24 @@
+﻿
+CREATE TABLE department
+(
+	id serial NOT NULL,
+	department_name text,
+	CONSTRAINT department_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE user_detail
+(
+	id serial NOT NULL,
+	username text,
+	date_added timestamp without time zone,
+	age integer,
+	department_id integer,
+	CONSTRAINT user_detail_pkey PRIMARY KEY (id),
+	CONSTRAINT "departFK" FOREIGN KEY (department_id)
+		REFERENCES department (id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE NO ACTION
+  );
+
+
+
